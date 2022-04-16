ee7f8ec800e943305ef5d2689a62fc29d9f2d206
kiwi-fruitiwi <zane.tian@gmail.com>
2022-04-16 11:15:20 -0700

    add template project: p5-typerc. add streets of new capenna JSON

---
+++ passage.js
@@ -0,0 +1,221 @@
+/*
+    displays a passage for typing, keeping track of correct and incorrect
+    letters by highlighting them
+
+    TODO
+        block on incorrect characters until correct one is entered
+ */
+
+class Passage {
+    constructor(text) {
+        this.text = text
+        this.index = 0 // where in the passage we're currently typing
+        this.correctList = [] // booleans recording character correctness
+
+        /*  keep track of where we last got a character incorrect; use this
+         to prevent marking a previously incorrect char correct once we succeed.
+         TODO this currently does not work because we skip incorrect chars :p
+         */
+        this.lastIncorrectIndex = -1
+    }
+
+
+    // renders this passage using vectors instead of constant offsets
+    render() {
+        noStroke()
+
+        let CHAR_POS = []
+
+        const TOP_MARGIN = 50
+        const LEFT_MARGIN = 25
+        const RIGHT_MARGIN = LEFT_MARGIN
+        const HIGHLIGHT_PADDING = 5
+
+
+        // the bottom left corner of the current letter we are typing = cursor
+        let cursor = new p5.Vector(LEFT_MARGIN, TOP_MARGIN)
+
+        let highlightTopLeftCorner = new p5.Vector()
+        const HIGHLIGHT_BOX_HEIGHT = textAscent() + textDescent() +
+            2*HIGHLIGHT_PADDING
+
+        /*  display the entire passage without text wrap
+         */
+        for (let i=0; i<this.text.length; i++) {
+            // save the position of the ith character. we'll need this later
+            CHAR_POS.push(cursor.copy())
+
+
+            /*  show the highlight box for correct vs incorrect after we type
+             */
+            if (i < this.index) {
+                if (this.correctList[i])
+                    fill(94, 100, 90, 15)
+                else
+                    fill(0, 100, 100, 20)
+
+                highlightTopLeftCorner.x = cursor.x
+                highlightTopLeftCorner.y = cursor.y - textAscent()
+
+                rect(
+                    highlightTopLeftCorner.x,
+                    highlightTopLeftCorner.y - HIGHLIGHT_PADDING,
+                    textWidth(this.text[i]),
+                    HIGHLIGHT_BOX_HEIGHT,
+                    2) // rounded rectangle corners
+            } else {
+                // don't draw a rect background if we haven't typed up to
+                // this index
+            }
+
+
+            /*  draw current letter above the highlight box in terms of z-index
+             */
+            fill(0, 0, 100, 70)
+            text(this.text[i], cursor.x, cursor.y)
+
+
+
+            /*  modify cursor position to where the next letter should be
+                each highlight box should be 1 pixel bigger on left and right
+                1+1=2 total pixels of extra width
+             */
+            cursor.x += textWidth(this.text[i]) + 2 // 2 = HORIZONTAL_PADDING
+
+            /*  let's do a simple word wrap, wrapping just by character!
+             */
+
+
+            // this is the horizontal coordinate where we must text wrap
+            const LINE_WRAP_X_POS = width - RIGHT_MARGIN
+
+            /*  if we're at a whitespace, determine if we need a new line:
+                    find the next whitespace
+                    the word between us and that whitespace is the next word
+                    if the width of that word + our cursor + current space >
+                     limit, then newline
+             */
+            if (this.text[i] === ' ') {
+                let ndi = this.text.indexOf(" ", i+1) // next delimiter index
+                let nextWord = this.text.substring(i+1, ndi)
+
+                if (textWidth(nextWord) +
+                    textWidth(this.text[i]) +
+                    cursor.x > LINE_WRAP_X_POS) {
+
+
+                    cursor.y += HIGHLIGHT_BOX_HEIGHT + 5
+
+                    // don't forget to wrap the x coordinates! ᴖᴥᴖ
+                    cursor.x = LEFT_MARGIN
+                }
+            }
+        }
+
+
+        /*  add current word top highlight horizontal bar
+         */
+        // find index of next and previous whitespace chars
+
+        // next delimiter index
+        let ndi = this.text.indexOf(" ", this.index)
+
+        // previous delimiter index
+        let pdi = this.text.lastIndexOf(" ", this.index)
+
+        // +1 because we don't want the line to go over the previous
+        // whitespace char
+        fill(0, 0, 80, 30) // gray
+
+
+        rect(
+            CHAR_POS[pdi+1].x, // start one char past the last delimiter
+            CHAR_POS[ndi].y - textAscent() - HIGHLIGHT_PADDING - 2,
+            // CHAR_POS[ndi].x - CHAR_POS[pdi].x
+            // this.textWidth*(ndi-pdi),
+            textWidth(this.text.substring(pdi+1, ndi+1)),
+            -2,
+            2)  // rounded rect corners
+
+        /*  add cursor below current character
+        */
+        fill(0, 0, 100)
+
+        // TODO check if we're finished, otherwise we try to read [index+1]
+        rect(
+            CHAR_POS[this.index].x,
+            CHAR_POS[this.index].y + textDescent(),
+            textWidth(this.text[this.index]),
+            2,
+            2)
+    }
+
+
+    getCurrentChar() {
+        return this.text[this.index]
+    }
+
+
+    // set the current char to correct
+    // TODO block on errors not supported
+    setCorrect() {
+        // if we've already gotten this char incorrect, don't add a correct
+        // value to correctList
+        if (this.lastIncorrectIndex === this.index) {
+            // do nothing
+        } else {
+            this.correctList.push(true)
+            this.incrementIndex()
+        }
+
+        console.assert(this.correctList.length === this.index)
+    }
+
+
+    // set the current char to be incorrect
+    // TODO block on errors not supported
+    setIncorrect() {
+        // only set incorrect for an index once!
+        if (this.lastIncorrectIndex !== this.index) {
+            this.correctList.push(false)
+            this.lastIncorrectIndex = this.index
+
+            /*
+                typingclub.com has two options:
+                    an incorrect advances the cursor, skipping the char
+                    an incorrect does not advance, requiring a correction
+
+                current code doesn't block on errors
+             */
+
+            this.incrementIndex()
+        }
+    }
+
+
+    decrementIndex() {
+        this.index -= 1
+    }
+
+
+    incrementIndex() {
+        this.index += 1
+    }
+
+
+    /*
+        string representation of correct and incorrect chars
+     */
+    toString() {
+        let correctRep = []
+        for (let c of this.correctList) {
+            if (c)
+                correctRep.push('.')
+            else
+                correctRep.push('*')
+        }
+
+        // TODO what is the equivalent of join in js?
+        return this.text + "\n" + correctRep.join('') + '\n'
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,137 @@
+/**
+ *  @author kiwi
+ *  @date 2022.04.16
+ *
+ *
+ */
+let font
+let instructions
+
+let passage
+let correctSound // audio cue for typing one char correctly
+let incorrectSound // audio cue for typing one char incorrectly
+
+
+
+function preload() {
+    font = loadFont('data/consola.ttf')
+}
+
+
+function setup() {
+    let cnv = createCanvas(640, 360)
+    cnv.parent('#canvas')
+    colorMode(HSB, 360, 100, 100, 100)
+
+    /* initialize instruction div */
+    instructions = select('#ins')
+    instructions.html(`<pre>
+        [1,2,3,4,5] → no function
+        numpad 1 → freeze sketch</pre>`)
+
+    colorMode(HSB, 360, 100, 100, 100)
+
+    correctSound = loadSound('data/correct.wav')
+    incorrectSound = loadSound('data/incorrect.wav')
+
+    passage = new Passage("Developers often work in teams, but it is not" +
+        " uncommon to find a developer who works independently as a" +
+        " consultant.  ")
+}
+
+
+function draw() {
+    background(234, 34, 24)
+    textFont(font, 30)
+    passage.render()
+
+    displayDebugCorner()
+}
+
+
+/** 🧹 shows debugging info using text() 🧹 */
+function displayDebugCorner() {
+    const LEFT_MARGIN = 10
+    const DEBUG_Y_OFFSET = height - 10 /* floor of debug corner */
+    const LINE_SPACING = 2
+    const LINE_HEIGHT = textAscent() + textDescent() + LINE_SPACING
+    fill(0, 0, 100, 100) /* white */
+    strokeWeight(0)
+
+    textFont(font, 14)
+    text(`frameCount: ${frameCount}`,
+        LEFT_MARGIN, DEBUG_Y_OFFSET - LINE_HEIGHT)
+    text(`frameRate: ${frameRate().toFixed(1)}`,
+        LEFT_MARGIN, DEBUG_Y_OFFSET)
+}
+
+
+function keyPressed() {
+    // don't do anything if we detect SHIFT ALT CONTROL keycodes
+    if (keyCode === SHIFT ||
+        keyCode === ALT ||
+        keyCode === CONTROL ||
+        keyCode === 20) { // this is capslock
+        return
+    }
+
+    /*  if the key we just pressed === passage.getCurrentChar, play correct
+        sound, rewind it, passage.setCorrect(). otherwise, play and rewind
+        the incorrect sound. passage.setIncorrect().
+     */
+    if (passage.getCurrentChar() === key) {
+        passage.setCorrect()
+        correctSound.play()
+    } else {
+        passage.setIncorrect()
+        incorrectSound.play()
+    }
+
+
+    /* stop sketch */
+    if (keyCode === 97) {
+        noLoop()
+        instructions.html(`<pre>
+            sketch stopped</pre>`)
+    }
+}
+
+
+
+
+/*
+@author Kiwi
+@date 2021-11-06
+
+This will be a typingclub clone made in the spirit of our 15.301 project
+ from 2004.
+
+
+planning
+    create passage class
+    add sound: correct and incorrect.wav via p5.sound play()
+
+
+commit schedule
+    console typing with truncated passage
+    don't clobber passage
+    display passage with text()
+    passage object ➜ highlights for correct vs incorrect
+    index underscore in blue
+    current gray horizontal marker
+    don't take modifier keys as input; allow capital letters
+
+planned features
+    multiline passage. wrap: find next word and test its width
+    reset button
+    bottom line indicating total progress. 100% on right
+    start typing animation: rectangle + bounce
+    tracking wpm
+    last word wpm
+    wpm and accuracy counter on bottom
+    scrolling text passage? before that we limit to screen size
+    scoring animation, maybe with camera. viewport
+        viewport https://forum.processing.org/two/discussion/14992/how-to-move-the-view-without-camera
+        cameras https://behreajj.medium.com/cameras-in-processing-2d-and-3d-dc45fd03662c
+
+ */
